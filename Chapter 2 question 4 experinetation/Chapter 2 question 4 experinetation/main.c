2f891aadb8dcdc0286ecfcce4f8537b46886e29b
Briguy2 <briansullivan2@gmail.com>
2014-02-03 16:52:11 -0600

    chapter 2 problem 4

---
+++ Chapter 2 question 4 experinetation/Chapter 2 question 4 experinetation/main.c
@@ -0,0 +1,18 @@
+/*Brian Sullivan
+//  main.c
+//  Chapter 2 question 4 experinetation
+//ANSWERS TO QUESTIONS FROM PROBLEM 4
+//
+A) TELLS PRINTF() TO EXPECT A FLOAT
+B) 
+ // Created by Programming on 2/3/14.
+//  Copyright (c) 2014 Programming. All rights reserved.
+*/
+
+#include <stdio.h>
+
+int main(){
+    float x =3.14159265;
+    printf ("floats: %d4.1f %4.2f %4.4f \n", x, x, x);
+}
+
